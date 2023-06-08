fc3e23aeddbcc3bb8ee40bfa718ce33e8ce1b8cd
GuuNguyem <guuhighschool@gmail.com>
2023-06-08 22:20:37 +0700

    get res by address login/register

---
+++ Repositories/DTOs/RestaurantDTO/GetAddressDTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.DTOs.RestaurantDTO
+{
+    public class GetAddressDTO
+    {
+        public int DistrictId { get; set; }
+        public int CityId { get; set; }
+    }
+}
---
+++ Repositories/Repositories/UserRepositories/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using BusinessObject.Models;
+using Repositories.DTOs.UserDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repositories.Repositories.UserRepositories
+{
+    public interface IUserRepository
+    {
+        User Login(LoginDTO user);
+        bool Register(CreateUserDTO user);
+    }
+}
